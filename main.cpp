19a15a9f76a97f90489b708c2255e978f472eda7
ascii-16 <mail.akhil99@gmail.com>
2025-07-18 22:35:38 +0530

    fix: Update list style(proper grid)

--- main.cpp
+++ main.cpp
@@ -51,16 +51,29 @@ int main()
         ImGui::InputText("Title", titleBuffer, IM_ARRAYSIZE(titleBuffer));
         ImGui::InputTextMultiline("Content", contentBuffer, IM_ARRAYSIZE(contentBuffer));
         ImGui::ColorEdit4("Color", (float *)&color);
-        ImGui::SliderFloat2("Position", (float *)&position, 0.0f, 500.0f);
 
         if (ImGui::Button("Add Task"))
         {
+            static float posX = 0.0f;
+            static float posY = 0.0f;
+            float padding = 10.0f;
+            float regionWidth = ImGui::GetContentRegionAvail().x;
+            ImVec2 cardSize = ImVec2(200, 100);
+
+            if (posX + cardSize.x > regionWidth)
+            {
+                posX = 0.0f;
+                posY += cardSize.y + padding;
+            }
+
             tasks.push_back(Task{
                 .title = titleBuffer,
                 .content = contentBuffer,
-                .position = position,
+                .position = ImVec2(posX, posY),
                 .color = color,
                 .completed = false});
+
+            posX += cardSize.x + padding;
 
             titleBuffer[0] = '\0';
             contentBuffer[0] = '\0';
