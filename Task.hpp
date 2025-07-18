8671e183a54944f6f2406542c0e4e2cdbe19246b
ascii-16 <mail.akhil99@gmail.com>
2025-07-18 22:16:40 +0530

    feat: Sample task

---
+++ Task.hpp
@@ -0,0 +1,11 @@
+#include <string>
+#include "imgui.h"
+
+struct Task
+{
+  std::string title;
+  std::string content;
+  ImVec2 position;
+  ImVec4 color;
+  bool completed;
+};
--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 #include <GLFW/glfw3.h>
+#include "Task.hpp"
 
 int main()
 {
+    Task task;
+
     if (!glfwInit())
         return -1;
 
@@ -15,15 +18,18 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Sample GUI app", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(1400, 900, "Sample GUI app", nullptr, nullptr);
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
     IMGUI_CHECKVERSION();
+
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
     (void)io;
+
     ImGui::StyleColorsDark();
+
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 150");
 
@@ -32,14 +38,24 @@ int main()
         glfwPollEvents();
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
 
-        ImGui::Begin("Hello!");
-        ImGui::Text("Submodules make life easier.");
+        ImGui::NewFrame();
+        ImGui::Begin("Task");
+        ImGui::Checkbox("completed", &task.completed);
+        ImGui::SameLine();
+        ImGui::Text("%s", task.title.c_str());
+
+        if (ImGui::Button("Delete"))
+        {
+            task.title = "[Deleted]";
+        }
+
         ImGui::End();
 
         ImGui::Render();
+
         int w, h;
+
         glfwGetFramebufferSize(window, &w, &h);
         glViewport(0, 0, w, h);
         glClearColor(0.1f, 0.15f, 0.2f, 1.0f);
