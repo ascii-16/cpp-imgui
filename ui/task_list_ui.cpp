0f7812c5579f37d24a2fe625c48aa3ec65a86fdc
ascii-16 <mail.akhil99@gmail.com>
2025-07-19 11:36:12 +0530

    feat: Update card style

--- ui/task_list_ui.cpp
+++ ui/task_list_ui.cpp
@@ -17,10 +17,9 @@ void delete_button(std::vector<Task> &tasks, std::string task_id) {
 }
 
 void render_task_list(std::vector<Task> &tasks, float cardWidth, float cardHeight) {
-    float padding = 10.0f;
+    float padding = 12.0f;
     float regionWidth = ImGui::GetContentRegionAvail().x;
-    float posX = 0.0f;
-    float posY = 0.0f;
+    float posX = 0.0f, posY = 0.0f;
 
     ImVec2 origin = ImGui::GetCursorScreenPos();
 
@@ -38,26 +37,47 @@ void render_task_list(std::vector<Task> &tasks, float cardWidth, float cardHeigh
         }
 
         ImVec2 cardPos = ImVec2(origin.x + posX, origin.y + posY);
-
         ImGui::SetCursorScreenPos(cardPos);
-        ImGui::PushStyleColor(ImGuiCol_ChildBg, task.color);
 
-        ImGui::BeginChild("TaskCard", ImVec2(cardWidth, cardHeight), true, ImGuiWindowFlags_NoMove);
-        ImGui::Text("%s", task.title.c_str());
-        ImGui::Separator();
+        ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 8.0f);
+        ImGui::PushStyleColor(ImGuiCol_ChildBg, ImVec4(task.color.x, task.color.y, task.color.z, 0.15f));
+        ImGui::PushStyleColor(ImGuiCol_Border, ImVec4(0.3f, 0.3f, 0.3f, 0.25f));
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(10, 8));
+
+        // Rounded border for card
+        ImDrawList *draw_list = ImGui::GetWindowDrawList();
+        ImU32 bgColor = ImGui::GetColorU32(task.color);
+        draw_list->AddRect(cardPos, ImVec2(cardPos.x + cardWidth, cardPos.y + cardHeight), bgColor, 10.0f, 0, 2.0f);
+        ImGui::SetCursorScreenPos(ImVec2(cardPos.x + 4, cardPos.y + 4));
+
+        // Card
+        ImGui::BeginChild("TaskCard", ImVec2(cardWidth - 8, cardHeight - 8), true,
+                          ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoSavedSettings);
+
+        ImGui::TextColored(ImVec4(1, 1, 1, 1), "%s", task.title.c_str());
+        ImGui::Spacing();
         ImGui::TextWrapped("%s", task.content.c_str());
 
+        ImGui::Dummy(ImVec2(0.0f, 8.0f));
+
+        // Footer row with checkbox and delete icon
+        ImGui::BeginGroup();
         if (ImGui::Checkbox("Done", &task.completed)) {
             update_task(tasks, task.id, task);
         }
+        ImGui::EndGroup();
 
+        ImGui::SameLine();
+        ImGui::SetCursorPosX(cardWidth - 24);
         delete_button(tasks, task.id);
 
+        // End card
         ImGui::EndChild();
-        ImGui::PopStyleColor();
+
+        ImGui::PopStyleVar(2);
+        ImGui::PopStyleColor(2);
 
         posX += cardWidth + padding;
-
         ImGui::PopID();
     }
 
--- ui/ui.hpp
+++ ui/ui.hpp
@@ -3,5 +3,5 @@
 #include <vector>
 #include "task.hpp"
 
-void render_task_list(std::vector<Task> &tasks, float cardWidth = 200.0f, float cardHeight = 150.0f);
+void render_task_list(std::vector<Task> &tasks, float cardWidth = 200.0f, float cardHeight = 100.0f);
 void add_task_button(std::vector<Task> &tasks, char *titleBuffer, char *contentBuffer, ImVec4 color);
