91941f0b16465b7bf1beb5809eabcaec446a4316
ascii-16 <mail.akhil99@gmail.com>
2025-07-20 14:32:06 +0530

    refactor: format

--- ui/add_task_form.cpp
+++ ui/add_task_form.cpp
@@ -16,13 +16,16 @@
  * @param contentBuffer Character buffer for the task content input field (mutable).
  * @param color The color associated with the new task.
  */
-void add_task_form(std::vector<Task> &tasks, char *titleBuffer, char *contentBuffer, ImVec4& color) {
+void add_task_form(std::vector<Task> &tasks, char *titleBuffer, char *contentBuffer, ImVec4 &color) {
     ImGui::InputText("Title", titleBuffer, IM_ARRAYSIZE(titleBuffer));
     ImGui::InputTextMultiline("Content", contentBuffer, IM_ARRAYSIZE(contentBuffer));
     ImGui::ColorEdit4("Color", (float *) &color);
     if (ImGui::Button("Add Task")) {
-        tasks.insert(tasks.begin(), Task{
-            .id = generate_uuid(), .title = titleBuffer, .content = contentBuffer, .color = color, .completed = false});
+        tasks.insert(tasks.begin(), Task{.id = generate_uuid(),
+                                         .title = titleBuffer,
+                                         .content = contentBuffer,
+                                         .color = color,
+                                         .completed = false});
 
         titleBuffer[0] = '\0';
         contentBuffer[0] = '\0';
